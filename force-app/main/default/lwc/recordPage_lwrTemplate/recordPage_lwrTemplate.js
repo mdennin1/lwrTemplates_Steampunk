50bfedd33284bcac06a62a8be81f461f5d2cb85e
matthew dennin <danny.dennin@steampunk.com>
2022-02-02 17:35:29 -0500

    lwr templates

---
+++ force-app/main/default/lwc/recordPage_lwrTemplate/recordPage_lwrTemplate.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * @slot header this is the headers slot
+ * @slot default this is the body/default slot
+ * @slot footer this is the footer slot
+ */
+
+export default class RecordPage_lwrTemplate extends LightningElement {}
---
+++ force-app/main/default/lwc/textDisplay_test/textDisplay_test.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class TextDisplay_test extends LightningElement {}
