3d703a96c4475e13e01d56c64e4cddb6e0abfe5a
matthew dennin <danny.dennin@steampunk.com>
2022-02-03 13:12:56 -0500

    removed unused lwcs

--- force-app/main/default/lwc/recordPage_lwrTemplate/recordPage_lwrTemplate.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-/**
- * @slot header this is the headers slot
- * @slot default this is the body/default slot
- * @slot footer this is the footer slot
- */
-
-export default class RecordPage_lwrTemplate extends LightningElement {}
