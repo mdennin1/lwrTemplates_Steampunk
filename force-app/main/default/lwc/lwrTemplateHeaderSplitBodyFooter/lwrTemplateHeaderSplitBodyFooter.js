cd4dd7a303acca7fb38b9a5dd714f0cd764e87c2
matthew dennin <danny.dennin@steampunk.com>
2022-02-03 13:12:31 -0500

    add lwrTemplateHeaderSplitBodyFooter

---
+++ force-app/main/default/lwc/lwrTemplateHeaderSplitBodyFooter/lwrTemplateHeaderSplitBodyFooter.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * @slot header header slot
+ * @slot left body left slot
+ * @slot right body right slot
+ * @slot footer footer slot
+ */
+
+export default class LwrTemplateHeaderSplitBodyFooter extends LightningElement {}
