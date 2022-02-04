7bb0407984d22192a9c07b50d136cc22fa71e206
matthew dennin <danny.dennin@steampunk.com>
2022-02-03 19:07:30 -0500

    added lwrTemplateHeaderSubheaderBodySIdebarRightFooter

---
+++ force-app/main/default/lwc/lwrTemplateHeaderSubheaderBodySidebarRightFooter/lwrTemplateHeaderSubheaderBodySidebarRightFooter.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+/**
+ * @slot header header slot
+ * @slot subheader subheader slot
+ * @slot default default content slot
+ * @slot sidebar sidebar slot
+ * @slot footer footer slot
+ */
+
+export default class LwrTemplateHeaderSubheaderBodySidebarRightFooter extends LightningElement {}
